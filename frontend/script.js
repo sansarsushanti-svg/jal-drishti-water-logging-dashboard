9d9686c45963f2969cf0d98e582f097295c45a0a
sansarsushanti-svg <sansarsushanti@gmail.com>
2026-01-04 01:27:40 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,23 @@
+const wardData = [
+    { ward: "Rajouri Garden", risk: "Red", status: "Severe" },
+    { ward: "Lajpat Nagar", risk: "Yellow", status: "Moderate" },
+    { ward: "Karol Bagh", risk: "Red", status: "Severe" },
+    { ward: "Dwarka", risk: "Green", status: "Resolved" },
+    { ward: "Rohini", risk: "Yellow", status: "Moderate" },
+    { ward: "Saket", risk: "Red", status: "Severe" }
+];
+
+const table = document.getElementById("wardTable");
+
+wardData.forEach(item => {
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+        <td>${item.ward}</td>
+        <td class="${item.risk.toLowerCase()}">${item.risk}</td>
+        <td>${item.status}</td>
+    `;
+
+    table.appendChild(row);
+});
 
